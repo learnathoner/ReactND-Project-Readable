8a64edeebeb650efcd764498bc8f79d0f54e8f02
learnathoner <mike.tikh@gmail.com>
2018-03-20 19:00:30 -0500

    Make title clickable, remove header title
    - Deleted Header title component
    - Made "Readable" title in header a link to home

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import CategoriesBar from './CategoriesBar'
-import HeaderTitle from './HeaderTitle'
+import React from 'react';
+import CategoriesBar from './CategoriesBar';
+import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="header">
-    <HeaderTitle />
+    <div className="title-container">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Link
+        to="/"
+        style={{
+          textDecoration: 'none',
+          color: 'white'
+        }}
+      >
+        <h1 className="App-title">Readable</h1>
+      </Link>
+    </div>
+
     <CategoriesBar />
   </div>
-)
+);
 
-export default Header
+export default Header;
